950b62becb8e58a11bd1ae27fdf4ce65b4b34e32
Torrell Williams <wtor11@yahoo.com>
2020-06-21 23:51:45 -0400

    Added comment for completion

--- bikeshare.py
+++ bikeshare.py
@@ -1,7 +1,7 @@
 import time
 import pandas as pd
 import numpy as np
-
+#Adding comment for completion
 # Importing CSV data 
 CITY_DATA = { 'chicago' : 'chicago.csv',
               'new york' : 'new_york_city.csv',
@@ -21,7 +21,7 @@ def get_filters():
     # Prompts user to select what city they want to pull data from and accounts for all user inputs
     while True:
                city = input("Which cities data would you like to see? Chicago, New York, Washington? \n").lower()
-                     
+
                if city == 'chicago':
                 print("You've selected Chicago's bike data to review.")
                 break
@@ -33,11 +33,11 @@ def get_filters():
                 break
                else:
                 print('\nInvalid input\n')
-                     
-                      
+
+
     # TO DO: get user input for month (all, january, february, ... , june)
     # Prompts user to select what month they want to pull data from and accounts for all user inputs
-    while True:                     
+    while True:
         month = input("\nWhich months data would you like to use? Choose between January - June, or all.\n").lower()
 
         if month == "all" or month =="january" or month =="february" or month =="march" or month =="april" or month =="may" or month =="june":
@@ -54,9 +54,9 @@ def get_filters():
         if day == "monday" or day =="tuesday" or day =="wednesday" or day =="thursday" or day =="friday" or day =="saturday" or day =="sunday" or day =="all":
             print("Let's look the data from {}.".format((day).title()))
             break
-        else: 
-            print("\nInvalid input.\n")         
-            
+        else:
+            print("\nInvalid input.\n")
+
     print('-'*40)
     #Returns user input to function
     return city, month, day
@@ -79,7 +79,7 @@ def load_data(city, month, day):
     df['Start Time']=pd.to_datetime(df['Start Time'])
     df['month']=df['Start Time'].dt.month
     df['weekday']=df['Start Time'].dt.weekday_name.str.lower()
-   
+
     # Prompts user if they would like to recieve additional raw data.
     #Nested in order to back out if need be.
     while True:
@@ -100,29 +100,29 @@ def load_data(city, month, day):
                                 elif raw3 == 'no':
                                     break
                                 else:
-                                    print("Invalid Input")      
-                                                                                        
+                                    print("Invalid Input")
+
                         break
                     elif raw2 == 'no':
                         break
                     else:
-                        print("Invalid Input")                                                
+                        print("Invalid Input")
             break
         elif raw == 'no':
             break
         else:
             print("Invalid Input")
-       
+
     #Selects specific month number plus 1 since array begins at 0
     if month != 'all':
-   
+
         months = ['january', 'february', 'march', 'april', 'may', 'june']
         month = months.index(month) + 1
 
         df = df[df['month'] == month]
-        
-    if day != 'all':        
-        df = df[df['weekday'] == day]                               
+
+    if day != 'all':
+        df = df[df['weekday'] == day]
     return df
 
 
@@ -217,11 +217,11 @@ def user_stats(df):
     #Scans data for greatest, least, and most repeated data for the column.
     while True:
         try:
-            early_yr = df['Birth Year'].min()    
-            recent_yr = df['Birth Year'].max()    
+            early_yr = df['Birth Year'].min()
+            recent_yr = df['Birth Year'].max()
             common_by = df['Birth Year'].mode()
             print('The earliest birth year is {} - The most recent birth rear is {} - The most common birth year is {}.'.format(int(early_yr), int(recent_yr), int(common_by)))
-            break      
+            break
         except KeyError:
             print('Birth Year data unavailable')
             break
